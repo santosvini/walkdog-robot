1482717c83e70c50874fd96dbe9b71e4afdada10
santosvini <vini_santos7@live.com>
2023-10-04 18:40:56 -0300

    Criando readme para documentacao do projeto, com ajustes finais do shell para execucao dos testes, com report incluido

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
+
 # Walkdog com Robot Framework
+
+Projeto de automação com ```Robot Framework``` com utilização de IA ```Chat-GPT```.
+
+Foram criados 10 testes de cadastros, 1 teste da home,
+e 1 teste de regressão com montagem de reports de forma automatizada através de um sistema ```shell```.
+
+## Instalação
+
+*Instalando o Python*
+
+[Python](https://python.org.br/instalacao-windows/)
+
+*Instalando o Pip*
+
+```bash
+  pip -m pip install --upgrade pip
+  pip -m pip --version
+```
+
+*Diretório do projeto*
+
+```bash
+  mkdir my-project
+  cd my-project
+```
+
+*Instalando as tecnologias e libs necessárias do projeto*
+
+[Instalação do Robot e SeleniumLibrary](https://github.com/robotframework/SeleniumLibrary/#installation), siga os comandos e após, caso seja do interesse, instale a lib faker
+
+```bash
+  pip install robotframework-faker
+```
+
+Para validar todas as instalações, 
+```bash
+  pip list / pip freeze
+```
+Criar req.txt com as 
+
+## Stacks utilizada
+
+**Libraries**: Builtin, Faker, SeleniumLibrary
+
+**Automação:** Robot Framework
+
+**Shell**: Shell Script
+
+**IA:** Chat-GPT
+
+## Rodando localmente
+
+Escolhe a opção desejada, espere a execução, após a mesma, retornará para o menu principal.
+
+![Screenshot](img\shell.png)
+
+Clone o projeto:
+
+```bash
+  git clone https://github.com/santosvini/walkdog-robot.git
+```
+
+Entre no diretório do projeto
+
+```bash
+  cd walkdog-robot
+```
+Para rodar os testes, rode o seguinte comando
+
+```bash
+  ./run.sh
+```
+
+## Resultados
+
+Nos testes de regressão, há o comando para gerar o report do allure, mas o mesmo pode ser gerado com o comando abaixo:
+
+```bash 
+allure serve
+```
+
+Aqui o report que o robot monta ao finalizar as execuções
+
+![Report](img\report.png)
+
+## Feedback
+
+Se você tiver algum feedback, por favor nos deixe saber por meio de [issues](https://github.com/santosvini/walkdog-robot/issues), neste repositório
+
+## Autor
+
+- [@santosvini](https://github.com/santosvini)
--- run.sh
+++ run.sh
@@ -1,84 +1,114 @@
 #!/bin/bash
-echo "Selecione uma opção:"
-echo "0 - Regressão"
-echo "1 - Test Case - Home"
-echo "2 - Test Case - Retorno a Home"
-echo "3 - Test Case - Cuidar"
-echo "4 - Test Case - Adestrar"
-echo "5 - Test Case - Ser um Dog Walker"
-echo "6 - Test Case - Campos Obrigatórios"
-echo "7 - Test Case - Cadastro sem nome"
-echo "8 - Test Case - Cadastro sem email"
-echo "9 - Test Case - Cadastro sem cpf"
-echo "10 - Test Case - Cadastro sem cep"
-echo "11 - Test Case - Numero Incorreto"
-echo "12 - Test Case - Sem documento"
 
-read opcao;
+echo "Vamos começar as execuções de testes..."
+
+option() {
+  echo "
+    0 - Regressão
+    1 - Test Case - Home
+    2 - Test Case - Retorno a Home
+    3 - Test Case - Cuidar
+    4 - Test Case - Adestrar
+    5 - Test Case - Ser um Dog Walker
+    6 - Test Case - Campos Obrigatórios
+    7 - Test Case - Cadastro sem nome
+    8 - Test Case - Cadastro sem email
+    9 - Test Case - Cadastro sem cpf
+    10 - Test Case - Cadastro sem cep
+    11 - Test Case - Numero Incorreto
+    12 - Test Case - Sem documento
+    13 - Exit....
+  "
+  
+  read -p "Escolheu a opção: " opcao
+
   case $opcao in
-  "0")
-  regression=`robot -d ./log tests/`
-  echo "$regression"
-  echo "Excutando testes de regressão..."
-  ;;
-  "1")
-   test_home=`robot -d ./log tests/home.robot`
-   echo "$test_home"
-   echo "Excutando o teste da home..."
-   ;;
-  "2")
-   test_case0=`robot -d ./log -i TC00 tests/cadastro.robot`
-   echo "$test_case0"
-   echo "Excutando o teste de retorno a home page..."
-   ;;
-  "3")
-   test_case1=`robot -d ./log -i TC01 tests/cadastro.robot`
-   echo "$test_case1"
-   echo "Excutando o teste para cuidar de um dog..."
-   ;;
-   "4")
-   test_case2=`robot -d ./log -i TC02 tests/cadastro.robot`
-   echo "$test_case2"
-   echo "Excutando o teste para adestrar um dog..."
-   ;;
-   "5")
-   test_case3=`robot -d ./log -i TC03 tests/cadastro.robot`
-   echo "$test_case3"
-   echo "Executando teste para ser um dogwalker..."
-   ;;
-   "6")
-   test_case4=`robot -d ./log -i TC04 tests/cadastro.robot`
-   echo "$test_case4"
-   echo "Executando o teste de campos obrigatórios..."
-   ;;
-   "7")
-   test_case5=`robot -d ./log -i TC05 tests/cadastro.robot`
-   echo "$test_case5"
-   echo "Executando o teste sem o nome completo..."
-   ;;
-   "8")
-   test_case6=`robot -d ./log -i TC06 tests/cadastro.robot`
-   echo "$test_case6"
-   echo "Executando o teste sem o email..."
-   ;;
-   "9")
-   test_case7=`robot -d ./log -i TC07 tests/cadastro.robot`
-   echo "$test_case7"
-   echo "Executando o teste sem o cpf..."
-   ;;
-   "10")
-   test_case8=`robot -d ./log -i TC08 tests/cadastro.robot`
-   echo "$test_case8"
-   echo "Executando o teste sem o cep..."
-   ;;
-   "11")
-   test_case9=`robot -d ./log -i TC09 tests/cadastro.robot`
-   echo "$test_case9"
-   echo "Executando o teste com número incorreto..."
-   ;;
-   "12")
-   test_case10=`robot -d ./log -i TC10 tests/cadastro.robot`
-   echo "$test_case10"
-   echo "Executando o teste sem documento..."
-
-esac
+    "0")
+      echo "Excutando testes de regressão..."
+      regression=`robot -d ./log --listener allure_robotframework:allure-results tests/`
+      
+      echo "Estou preparando o seu relatório...."
+      
+      allure serve allure-results
+      
+      echo "$regression"
+      option
+    ;;
+    "1")
+      echo "Excutando o teste da home..."
+      test_home=`robot -d ./log tests/home.robot`
+      echo "$test_home"
+      option
+    ;;
+    "2")
+      echo "Excutando o teste de retorno a home page..."
+      test_case0=`robot -d ./log -i TC00 tests/cadastro.robot`
+      echo "$test_case0"
+      option
+    ;;
+    "3")
+      echo "Excutando o teste para cuidar de um dog..."
+      test_case1=`robot -d ./log -i TC01 tests/cadastro.robot`
+      echo "$test_case1"
+      option
+    ;;
+    "4")
+      echo "Excutando o teste para adestrar um dog..."
+      test_case2=`robot -d ./log -i TC02 tests/cadastro.robot`
+      echo "$test_case2"
+      option
+    ;;
+    "5")
+      echo "Executando teste para ser um dogwalker..."
+      test_case3=`robot -d ./log -i TC03 tests/cadastro.robot`
+      echo "$test_case3"
+      option
+    ;;
+    "6")
+      echo "Executando o teste de campos obrigatórios..."
+      test_case4=`robot -d ./log -i TC04 tests/cadastro.robot`
+      echo "$test_case4"
+      option
+    ;;
+    "7")
+      echo "Executando o teste sem o nome completo..."
+      test_case5=`robot -d ./log -i TC05 tests/cadastro.robot`
+      echo "$test_case5"
+      option
+    ;;
+    "8")
+      echo "Executando o teste sem o email..."
+      test_case6=`robot -d ./log -i TC06 tests/cadastro.robot`
+      echo "$test_case6"
+      option
+    ;;
+    "9")
+      echo "Executando o teste sem o cpf..."
+      test_case7=`robot -d ./log -i TC07 tests/cadastro.robot`
+      echo "$test_case7"
+      option
+    ;;
+    "10")
+      echo "Executando o teste sem o cep..."
+      test_case8=`robot -d ./log -i TC08 tests/cadastro.robot`
+      echo "$test_case8"
+      option
+    ;;
+    "11")
+      echo "Executando o teste com número incorreto..."
+      test_case9=`robot -d ./log -i TC09 tests/cadastro.robot`
+      echo "$test_case9"
+      option
+    ;;
+    "12")
+      echo "Executando o teste sem documento..."
+      test_case10=`robot -d ./log -i TC10 tests/cadastro.robot`
+      echo "$test_case10"
+      option
+    ;;
+    "13")
+      echo "Saindo do menu de execução..."
+      exit 0
+  esac
+}
+option
